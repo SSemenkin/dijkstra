cd0629e514efca9e68b878f72bf7b50eadd3ef69
SSemenkin <linkforptr@gmail.com>
2021-10-30 18:10:03 +0300

    win cmake

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 #include <fstream>
 
-#include <myvector/vector.h>
+#include "myvector/vector.h"
+
+
+int main(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+
+    bicycle::Vector<int> m_data;
+
+    for (int i = 0;i < 10; ++i) {
+        m_data.push_back(i);
+    }
+
+    for (int i : m_data) {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
